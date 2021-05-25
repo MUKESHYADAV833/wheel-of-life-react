6aabc44698555f6b183bf0b4106e5bf92a91f068
mukeshyadav <mukeshyadav20000229@gmail.com>
2021-05-25 08:05:53 +0530

    working on new ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import 'font-awesome/css/font-awesome.min.css';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state =null
+    this.state = null
   }
-  
+
+  showdataentry() {
+    console.log("haii");
+  }
+
+
   render() {
     return (
-      <div className="header">
-      <div className="head">Wheel Of Life</div>
-     <div> <i className="fab fa-amazon"></i></div>
-      {/* <div><i className="fa fa-spinner fa-spin">no spinner but why</i></div> */}
+      <div>
+        <div className="header">
+          <img id="logo" src='./wings2life.png' />
+          <div className="head">Wheel Of Life</div>
+        </div>
+        <div className="parent">
+          <div className="t">
+            <div id="l1" className="tiles" onClick={this.showdataentry}>Friends and Family:</div>
+            <div id="l2" className="tiles" onClick={this.showdataentry}>Relationships:</div>
+            <div id="l3" className="tiles" onClick={this.showdataentry}>Wealth:</div>
+            <div id="l4" className="tiles" onClick={this.showdataentry}>Personal Growth:</div>
+          </div>
+          <div className="t1">
+            <div id="l5" className="tiles1" onClick={this.showdataentry}>Health:</div>
+            <div id="l6" className="tiles1" onClick={this.showdataentry}>Fun and Recreation</div>
+            <div id="l7" className="tiles1" onClick={this.showdataentry}>Possesion:</div>
+            <div id="l8" className="tiles1" onClick={this.showdataentry}>Career:</div>
+          </div>
+        </div>
       </div>
 
     );
@@ -20,3 +39,4 @@ class Header extends React.Component {
 }
 
 export default Header;
+
