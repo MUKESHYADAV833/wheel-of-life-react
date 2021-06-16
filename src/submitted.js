2ff6d5852d73baf4157431c2d71e34fd6cdc8bd9
mukeshyadav <mukeshyadav20000229@gmail.com>
2021-06-16 16:56:50 +0530

    solve git conflict

--- src/submitted.js
+++ src/submitted.js
@@ -1,9 +1,5 @@
-import React from 'react';
-<<<<<<< HEAD
+import React from 'react';s
 import { Polar} from 'react-chartjs-2';
-=======
-import { Polar } from 'react-chartjs-2';
->>>>>>> 9e99ac16aee8264cc82c0f9f1ec1ca3bc6bf7d36
 
 
 class Submitted extends React.Component {
@@ -31,58 +27,16 @@ class Submitted extends React.Component {
                         'orange',
                         'green'
                     ],
-<<<<<<< HEAD
                     data: this.props.dropped
                 }
             ]
            
-=======
-                    //   hoverBackgroundColor: [
-                    //   '#501800',
-                    //   '#4B5000',
-                    //   '#175000',
-                    //   '#003350',
-                    //   '#35014F'
-                    //   ],
-                    data: this.props.dropped
-                    // data: [2, 4, 6, 8, 10]
-                }
-            ]
-            // maintainAspectRatio: false,
-            // responsive: false,
-            // labels: ["a", "b", "c", "d"],
-            // datasets: [
-            //     {
-            //         data: this.props.dropped
-            //     }
-            // ]
->>>>>>> 9e99ac16aee8264cc82c0f9f1ec1ca3bc6bf7d36
         }
     }
 
     render() {
-<<<<<<< HEAD
         console.log(this.state);
         return (
-       
-            <Polar
-                data={this.state}
-                options={{
-                    legend: {
-                        display: true,
-                        position: 'right'
-                    },
-                    scale:{
-                        display:false
-=======
-        // const { data } = this.state.datasets;
-        // data = this.props.dropped.map(this.props.dropped);
-        // this.setState({ data });
-        console.log(this.state);
-        return (
-            // <svg width="100" height="100">
-            //     <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-            // </svg>
             <Polar
                 data={this.state}
                 options={{
@@ -94,7 +48,6 @@ class Submitted extends React.Component {
                     legend: {
                         display: true,
                         position: 'right'
->>>>>>> 9e99ac16aee8264cc82c0f9f1ec1ca3bc6bf7d36
                     }
                 }}
             />
