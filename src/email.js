f08e5dd3953b999fd3ca0e1d2058b1705c32ddb4
KHYATI SHARMA <khyatisharma2001514@gmail.com>
2021-06-19 14:16:23 +0530

    working on the modal to send email

--- src/email.js
+++ src/email.js
@@ -4,23 +4,26 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function MyVerticallyCenteredModal(props) {
-  let handleOnChange = ( email ) => {
+  let handleOnChange = (email) => {
 
     // don't remember from where i copied this code, but this works.
     let re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if ( re.test(email) ) {
-        // this is a valid email address
-        // call setState({email: email}) to update the email
-        // or update the data in redux store.
-        alert("successful")
+    if (re.test(email)) {
+      // this is a valid email address
+      // call setState({email: email}) to update the email
+      // or update the data in redux store.
+      alert("successful")
     }
     else {
-        // invalid email, maybe show an error to the user.
-        alert("haii")
+      // invalid email, maybe show an error to the user.
+      alert("haii")
     }
 
-}
+  }
+
+  const [value, setValue] = useState();
+
   return (
 
     <Modal
@@ -36,11 +39,11 @@ export default function MyVerticallyCenteredModal(props) {
       </Modal.Header>
       <Modal.Body>
         <p>Give your mail below so that we can send you a copy of your WHEEL OF LIFE</p>
-        <input type="email"id="phone"  placeholder="abc@gmail.com"/>
+        <input type="email" id="phone" onSubmit={e => setValue(e.target.value)} placeholder="abc@gmail.com" />
 
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide, handleOnChange}>Done</Button>
+        <Button onClick={props.onHide}>Done</Button>
       </Modal.Footer>
     </Modal>
   );
